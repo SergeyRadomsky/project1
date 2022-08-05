d2a90c42a319dbf0ff31e048e432e045f2548304
SergeyRadomsky <radomskijs@mail.ru>
2022-08-05 13:22:37 +0300

    firts try and commit

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict"
+
+let money = prompt("Ваш бюджет на месяц");
+let time = prompt("Ваш бюджет на месяц");
+
+let appData = {
+    budget: money,
+    expenses: {},
+    income: [],
+    optionalExpenses: {},
+    timeData: time,
+    savings: false,
+};
+
+let a1 = prompt("Введите обязательную стать расходов", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную стать расходов", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+console.log(appData.budget / 30);
+
+
+
